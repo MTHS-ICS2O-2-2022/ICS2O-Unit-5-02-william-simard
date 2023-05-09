2e2e79baab903e8818de554c4826c2b6d56f95b7
william <124389603+william-simard@users.noreply.github.com>
2023-05-09 17:13:40 +0000

    work done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 william simard All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: wiilliam simard
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById("on-check").checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: On</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "<p>Value is: Off</p>"
+  }
+}
